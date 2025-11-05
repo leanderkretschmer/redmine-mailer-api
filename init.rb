0fcaa45fad9ea23c1fee13eb93a8e63390d81d20
leanderkretschmer <leanderkretschmer@icloud.com>
2025-11-05 12:29:44 +0100

    updated routes

--- init.rb
+++ init.rb
@@ -14,3 +14,17 @@ Redmine::Plugin.register :redmine_mailer_api do
   requires_redmine version_or_higher: '6.0.0'
 end
 
+# Routes manuell registrieren, um sicherzustellen, dass sie korrekt geladen werden
+Rails.application.config.to_prepare do
+  Rails.application.routes.draw do
+    # Route für E-Mail-Suche muss vor den user_id-basierten Routes stehen
+    match 'users/mails(.:format)', :to => 'user_mails#search', :via => [:get], :as => 'search_user_mail'
+    
+    match 'users/:user_id/mails(.:format)', :to => 'user_mails#index', :via => [:get], :as => 'user_mails'
+    match 'users/:user_id/mails(.:format)', :to => 'user_mails#create', :via => [:post]
+    match 'users/:user_id/mails/:id(.:format)', :to => 'user_mails#show', :via => [:get], :as => 'user_mail'
+    match 'users/:user_id/mails/:id(.:format)', :to => 'user_mails#update', :via => [:put, :patch]
+    match 'users/:user_id/mails/:id(.:format)', :to => 'user_mails#destroy', :via => [:delete]
+  end
+end
+
